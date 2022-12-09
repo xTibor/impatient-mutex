f0b09099e5421165448ffd2aa94d8e0321e5f717
Nagy Tibor <xnagytibor@protonmail.com>
2022-12-09 01:20:26 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use std::sync::{LockResult, Mutex, MutexGuard};
+use std::thread;
+use std::time::Duration;
+
+struct ImpatientMutex<T> {
+    mutex: Mutex<T>,
+}
+
+impl<T> ImpatientMutex<T> {
+    pub fn new(t: T) -> Self {
+        Self {
+            mutex: Mutex::new(t),
+        }
+    }
+
+    pub fn lock(&self) -> LockResult<MutexGuard<'_, T>> {
+        for _ in 0..=10 {
+            if let Ok(result) = self.mutex.try_lock() {
+                return Ok(result);
+            }
+
+            thread::sleep(Duration::from_millis(100));
+        }
+
+        panic!("impatient mutex");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ImpatientMutex;
+
+    #[test]
+    fn test_01() {
+        let mutex = ImpatientMutex::new(true);
+
+        let x = mutex.lock();
+        let y = mutex.lock();
+    }
+}
